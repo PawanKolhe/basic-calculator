249d7d127a475cc3614ec7c65649b61698b4b92b
Pawan Kolhe <contact@pawankolhe.com>
2020-02-03 20:03:52 +0530

    bugs fixes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Stores state of calculator
 let calc = {
   num1: '0',
   num2: '0',
@@ -5,28 +6,10 @@ let calc = {
   operator: '+',
   expression: '',
   result: '',
-  numbers: {
-    nine: document.querySelector('.calc-button[data-name="nine"]'),
-    eight: document.querySelector('.calc-button[data-name="eight"]'),
-    seven: document.querySelector('.calc-button[data-name="seven"]'),
-    six: document.querySelector('.calc-button[data-name="six"]'),
-    five: document.querySelector('.calc-button[data-name="five"]'),
-    four: document.querySelector('.calc-button[data-name="four"]'),
-    three: document.querySelector('.calc-button[data-name="three"]'),
-    two: document.querySelector('.calc-button[data-name="two"]'),
-    one: document.querySelector('.calc-button[data-name="one"]'),
-    zero: document.querySelector('.calc-button[data-name="zero"]')
-  },
+  inputs: document.querySelectorAll('.input'),
   action: {
     active: true,
-    operators: {
-      div: document.querySelector('.calc-button[data-name="div"]'),
-      mul: document.querySelector('.calc-button[data-name="mul"]'),
-      sub: document.querySelector('.calc-button[data-name="sub"]'),
-      add: document.querySelector('.calc-button[data-name="add"]'),
-      root: document.querySelector('.calc-button[data-name="root"]'),
-      exp: document.querySelector('.calc-button[data-name="exp"]')
-    }
+    operators: document.querySelectorAll('.operator')
   },
   equal: document.querySelector('.calc-button[data-name="equal"]'),
   equalLast: false,
@@ -43,7 +26,7 @@ const changeSymbolsToNormal = (s) => s.replace('÷', '/').replace('×', '*').rep
 // Change expression symbols from normal to unicode
 const changeSymbolsToUnicode = (s) => s.replace('/', '÷').replace('*', '×').replace('-', '−').replace('+', '+');
 
-// Update functions
+// Display update functions
 const updateDisplayExpression = () => {
   if(calc.equalLast === true){
     calc.displayExpression.innerText = calc.expression + ' =';
@@ -53,13 +36,19 @@ const updateDisplayExpression = () => {
 }
 const updateDisplayMain = () => {
   if(!calc.num2Active) {
-    calc.displayMain.innerText = calc.num1;
+    calc.displayMain.innerText = calc.num1.toLocaleString('en-US', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 8
+    });
   } else {
-    calc.displayMain.innerText = calc.num2;
+    calc.displayMain.innerText = calc.num2.toLocaleString('en-US', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 8
+    });
   }
 }
 
-// Clear all displays
+// Clear all displays and reset values to initial
 const clearDisplay = () => {
   calc.num1 = '0';
   calc.num2 = '0';
@@ -68,59 +57,53 @@ const clearDisplay = () => {
   calc.expression = '';
   calc.result = '';
   calc.action.active = true;
+  calc.equalLast = false;
   updateDisplayExpression();
   updateDisplayMain();
 }
 
-const calculate = (operator) => {
+// Perform calculation based on given operator and two numbers
+const calculate = (operator, num1, num2) => {
   switch(operator) {
     case '+':
-      calc.result = parseFloat(calc.num1) + parseFloat(calc.num2);
+      calc.result = parseFloat(num1) + parseFloat(num2);
       break;
     case '-':
-      calc.result = parseFloat(calc.num1) - parseFloat(calc.num2);
+      calc.result = parseFloat(num1) - parseFloat(num2);
       break;
     case '*':
-      calc.result = parseFloat(calc.num1) * parseFloat(calc.num2);
-      calc.result = calc.result.toLocaleString('en-US', {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 8
-      });
+      calc.result = parseFloat(num1) * parseFloat(num2);
       break;
     case '/':
-      calc.result = parseFloat(calc.num1) / parseFloat(calc.num2);
-      calc.result = calc.result.toLocaleString('en-US', {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 8
-      });
+      calc.result = parseFloat(num1) / parseFloat(num2);
       break;
     case '^':
-      calc.result = parseFloat(calc.num1) ** parseFloat(calc.num2);
-      calc.result = calc.result.toLocaleString('en-US', {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 8
-      });
+      calc.result = parseFloat(num1) ** parseFloat(num2);
       break;
   }
 
   return calc.result;
 }
 
-// Loop through number buttons, add click listener, and update Main Display
-Object.entries(calc.numbers).forEach(([key, value]) => {
-  calc.numbers[key].addEventListener('click', () => {
+// Add click listener to each input, and update Main Display
+calc.inputs.forEach(element => {
+  element.addEventListener('click', function() {
+    if(calc.equalLast === true) {
+      clearDisplay();
+    }
+
     calc.action.active = false;
     if(!calc.num2Active) {
       if(calc.num1 == '0') {
         calc.num1 = '';
       }
-      calc.num1 += value.value;
+      calc.num1 += this.value;
     } 
     else {
       if(calc.num2 == '0') {
         calc.num2 = '';
       }
-      calc.num2 += value.value;
+      calc.num2 += this.value;
     }
     updateDisplayMain()
   });
@@ -141,15 +124,15 @@ calc.dot.addEventListener('click', () => {
   updateDisplayMain()
 });
 
-// Loop through operator buttons, add click listener, and update Operator Display
-Object.entries(calc.action.operators).forEach(([key, value]) => {
-  calc.action.operators[key].addEventListener('click', () => {
+// Add click listener to each operator, and update Expression Display
+calc.action.operators.forEach(element => {
+  element.addEventListener('click', function() {
     if(!calc.num2Active) {
       calc.num1 = calc.num1.replace(/\.+$/, '');
     } else {
       calc.num2 = calc.num2.replace(/\.+$/, '');
     }
-
+  
     if(calc.action.active === true) {
       let temp_str;
       if(calc.expression == "") {
@@ -162,54 +145,68 @@ Object.entries(calc.action.operators).forEach(([key, value]) => {
       }
       
       if(calc.equalLast === true) {
-        calc.expression = `${calc.result} ${value.value} `;
+        calc.expression = `${calc.result} ${this.value} `;
         calc.equalLast = false;
         calc.num2 = '0';
       } else {
-        calc.expression = `${temp_str} ${value.value} `;
+        calc.expression = `${temp_str} ${this.value} `;
       }
       
       updateDisplayExpression();
     } 
     else {
-      calc.expression += `${calc.num2Active ? calc.num2 : calc.num1} ${value.value} `;
+      calc.expression += `${calc.num2Active ? calc.num2 : calc.num1} ${this.value} `;
       updateDisplayExpression();
-
-      try {
-        calc.result = calculate(changeSymbolsToNormal(calc.operator));
-
-        calc.displayMain.innerText = String(calc.result);
-        calc.num1 = String(calc.result);
-        
-        calc.num2 = '0';
-      }
-      catch(err) {
-        console.log(err.message);
-        console.log(calc);
-      }
+  
+      calc.result = calculate(changeSymbolsToNormal(calc.operator), calc.num1, calc.num2);
+      calc.displayMain.innerText = calc.result.toLocaleString('en-US', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 8
+      });
+      calc.num1 = String(calc.result);
+      
+      calc.num2 = '0';
     }
-
+  
+    // if true, indicates operator button was the last button pressed
     calc.action.active = true;
+
+    // if true, context of input buttons pressed changes to num2
     calc.num2Active = true;
-    calc.operator = value.value;
-    
+
+    // sets the operator
+    calc.operator = this.value;
   });
 });
 
+
+// = Button
 calc.equal.addEventListener('click', () => {
   calc.equalLast = true;
 
-  calc.expression = `${calc.result} ${calc.operator} ${calc.num2}`;
-  updateDisplayExpression();
-
-  calc.result = calculate(changeSymbolsToNormal(calc.operator));
-  calc.displayMain.innerText = String(calc.result);
-  calc.num1 = String(calc.result);
-
+  // update expression and perform calculation
+  if(calc.num2 == '') {
+    calc.expression = `${calc.result} ${calc.operator} ${calc.num1}`;
+    updateDisplayExpression();
+    calc.result = calculate(changeSymbolsToNormal(calc.operator), calc.result, calc.num1);
+  } else {
+    calc.expression = `${calc.result} ${calc.operator} ${calc.num2}`;
+    updateDisplayExpression();
+    calc.result = calculate(changeSymbolsToNormal(calc.operator), calc.num1, calc.num2);
+  }
+  
+  // updates main display to show result
+  calc.displayMain.innerText = calc.result.toLocaleString('en-US', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 8
+  });
+  
+  calc.num1 = calc.result;
   calc.action.active = true;
   calc.num2Active = true;
 });
 
+// C Button
 calc.clear.addEventListener('click', () => {
   clearDisplay();
 });
